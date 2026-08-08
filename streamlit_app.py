25a0edeb66425f6dcb4847acb4e4148402fcdabf
kasaandraquispe1-design <kasaandraquispe1@gmail.com>
2026-08-08 09:31:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import GoogleTranslator
+from deep_translator import GoogleTranslator
 # =========================================================
 # CONFIGURACIÓN
 # =========================================================
